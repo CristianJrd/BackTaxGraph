fc48a0409ca7217760a05ee176dd88f7cf7201b1
Cristian Contreras <jes__22222@hotmail.com>
2018-09-25 11:26:27 -0500

    change server

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,6 @@ export const pubsub = new PubSub();
 //     return /*date + '/' + month + '/' + year + ' ' + */hr + ':' + m.substr(-2)/* + ':' + s.substr(-2)*/;
 // }
 
-const app = express();
 const PORT = process.env.PORT || 4000
 // Load environment variables from .env file, where API keys and passwords are configured
 // dotenv.config({ path: ".env" });
@@ -50,84 +49,11 @@ mongoose.connect(`mongodb://cristian:1q2w3e4r5t6y@ds125932.mlab.com:25932/iotaxi
 const db = mongoose.connection;
 db.on('error',()=> console.log("Error al conectar a la BD"))
 	.once('open',() => console.log("Conectado a la BD!!"))
-	
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
  const schema: GraphQLSchema = mergeSchemas({
 	schemas,
 	resolvers
 });
-
-app.use('/graphql',graphQLHTTP((req,res)=>({
-    schema,
-    graphiql:true,
-    pretty:true,
-    context:{
-        user:req.headers
-    }
-})))
-
-app.post('/createMessage',(req,res) => {
-
-    const message = req.body
-    console.log(message)
-    Message.create(message).then((message) => {
-        console.log(message,"aqui chido")
-/*         let hora = Unix_timestamp(message.timestamp);
-        console.log(hora)
-
-        if(hora>= '4:00' && hora <='5:00'){
-            console.log("ENTRO RESET")
-            Device.findByIdAndUpdate(message.device,{$set:{contEfectivo:0, contKm:0, contTime:0, contTravel:0}},(err,dev) => {
-                return dev
-            })
-        }
-
-        if(message.data.length === 12){
-            if(message.data.indexOf('00') === 0 || message.data.indexOf('01') === 0){
-                console.log("entro")
-                let pesos = message.data.substr(0,4);
-                let cent = message.data.substr(4,2);
-                let cash = Number(pesos+"."+cent);
-                let km = Number(message.data.substr(6,3));
-                let time = Number(message.data.substr(9,3));
-                console.log(cash,",",km,",",time)
-                Device.findByIdAndUpdate(message.device,{$inc:{contEfectivo:cash, contKm:km, contTime:time, contTravel:1}},(err,dev) => {
-                    return dev
-                })
-                console.log("salio")
-
-            }else{
-                Device.findByIdAndUpdate(message.device,{$set:{lastLocation:message.data}},(err,dev) => {
-                    return dev
-                })
-            }
-        } */
-        return res.status(201).json({"message":"Mensaje creado", "id":message._id})
-    }).catch((err)=>{
-        console.log(err);
-        return res.json(err)
-    }) 
-})
-
-app.post('/addDevice',(req,res) => {
-    const device = req.body
-    console.log(device)
-    Device.create(device).then((device) => {
-        return res.status(201).json({"message":"Dispositivo Creado","id":device._id})
-    }).catch((err)=>{
-        console.log(err);
-        return res.json(err)
-    })
-
-})
-
-app.get('/',(req,res) => {
-	res.send("Estoy funcionando :)")
-	schema
-})
 
 // GraphQL
 const server = new ApolloServer({
